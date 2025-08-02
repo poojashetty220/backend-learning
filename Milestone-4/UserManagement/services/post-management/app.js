da0d1b0ccb41062def7f3b4c87a1c459b892a95e
poojashetty <pooja.shetty@7edge.com>
2025-08-02 21:50:43 +0530

    Cleaning up

--- Milestone-4/UserManagement/services/post-management/app.js
+++
@@ -1,9 +0,0 @@
-// app.js (as ES Module)
-import express from 'express';
-import posts from './routes/posts.js';
-
-const router = express.Router();
-
-router.use('/', posts);
-
-export default router;
--- Milestone-4/UserManagement/services/user-management/app.js
+++
@@ -1,9 +0,0 @@
-// app.js (as ES Module)
-import express from 'express';
-import users from './routes/users.js';
-
-const router = express.Router();
-
-router.use('/users', users);
-
-export default router;
